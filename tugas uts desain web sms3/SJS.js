3714ad2e0b523b88426fd5e364217d70e1e5da76
nyambekgoreng <alfitoadityat@gmail.com>
2024-11-17 21:21:34 +0700

    Delete tugas uts desain web sms3 directory

--- tugas uts desain web sms3/SJS.js
+++
@@ -1,6 +0,0 @@
-function showSection(section) {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(sec => sec.style.display = 'none');
-
-    document.getElementById(section).style.display = 'block';
-}
